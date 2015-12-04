0f1e7cb483bc9a71d2a000aeb22548f006face48
Matt Congel <congel@chapman.edu>
2015-12-04 14:02:45 -0800

    added testimonial carousel

---
+++ assets/javascripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#testimonial-carousel").owlCarousel({
+    singleItem : true,
+    stopOnHover: true,
+    navigation: false,
+    autoPlay: true,
+    autoHeight: true,
+    pagination: true,
+    paginationNumbers: false
+  });
+});
